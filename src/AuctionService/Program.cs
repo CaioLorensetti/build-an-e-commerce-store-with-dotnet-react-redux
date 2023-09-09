a70969457ddd74718696a6aa33e5062addb97641
Caio Lorensetti <caiolorensetti@gmail.com>
2023-09-09 01:31:19 -0300

    create dtos and mappingprofile

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -8,7 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<AuctionDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("AuctionContext")));
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
---
+++ src/AuctionService/RequestHelper/MappingProfiles.cs
@@ -0,0 +1,18 @@
+﻿using AuctionService.DTOs;
+using AuctionService.Entities;
+using AutoMapper;
+
+namespace AuctionService.RequestHelper;
+
+public class MappingProfiles : Profile
+{
+    public MappingProfiles()
+    {
+        CreateMap<Auction, AuctionDto>().IncludeMembers(x => x.Item);
+        CreateMap<Item, AuctionDto>();
+        CreateMap<CreateAuctionDto, Auction>()
+        .ForMember(dest => dest.Item, opt => opt.MapFrom(src => src));
+        CreateMap<CreateAuctionDto, Item>();
+    }
+
+}
