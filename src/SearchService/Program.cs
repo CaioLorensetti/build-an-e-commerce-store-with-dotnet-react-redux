c08681aa750141b276714553e79b250dea331990
Caio Lorensetti <caiolorensetti@gmail.com>
2023-09-10 18:12:48 -0300

    initialize mongodb searchdb

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,3 +1,7 @@
+using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -19,5 +23,14 @@ if (app.Environment.IsDevelopment())
 app.UseAuthorization();
 
 app.MapControllers();
+
+await DB.InitAsync("SearchDB", MongoClientSettings
+    .FromConnectionString(builder.Configuration.GetConnectionString("MongoDBConnection")));
+
+await DB.Index<Item>()
+    .Key(x => x.Make, KeyType.Text)
+    .Key(x => x.Model, KeyType.Text)
+    .Key(x => x.Color, KeyType.Text)
+    .CreateAsync();
 
 app.Run();
