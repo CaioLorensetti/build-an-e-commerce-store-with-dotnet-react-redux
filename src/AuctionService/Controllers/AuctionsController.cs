7b9523707aa6867f5f4b395cdc11cd87c7fa77b6
Caio Lorensetti <caiolorensetti@gmail.com>
2023-09-10 16:46:49 -0300

    delete auctionapi

--- src/AuctionService/Controllers/AuctionsController.cs
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -86,4 +86,24 @@ public class AuctionsController : ControllerBase
         }
         return Ok();
     }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> DeleteAuction(Guid id)
+    {
+        var auction = await _context.Auctions.FindAsync(id);
+        if (auction == null)
+        {
+            return NotFound();
+        }
+
+        // TODO: check seller = username
+
+        _context.Auctions.Remove(auction);
+        var result = await _context.SaveChangesAsync() > 0;
+        if (!result)
+        {
+            return BadRequest("Could not save changes.");
+        }
+        return Ok();
+    }
 }
